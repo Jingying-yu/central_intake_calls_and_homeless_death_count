89f8607ebca5f681accbd9b38c304fc548f8bac0
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-01-17 21:25:03 +0000

    Edited path to download data

--- central_intake_calls_and_homeless_death-main/scripts/01-download_data.R
+++ central_intake_calls_and_homeless_death-main/scripts/01-download_data.R
@@ -14,7 +14,23 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Data of Central Intake Calls received that were assigned a wrap-up code 
+# for tracing and follow-up purposes
+raw_central_intake_call_wrap_up <-
+  read_csv(
+    file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/central-intake-calls/resource/4858b2dc-9b4e-425c-aaeb-ef23d819cbc4/download/Central%20Intake%20Call%20Wrap-Up%20Codes%20Data.csv",
+    show_col_types = FALSE,
+    skip = 1
+  )
+
+# Data of Homeless Death Counts in Toronto (by month)
+raw_homeless_death_counts <-
+  read_csv(
+    file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/dc4ec2fa-d655-46ca-af32-f216d26e9804/download/Homeless%20deaths%20by%20month.csv",
+    show_col_types = FALSE,
+    skip = 1
+  )
+
 
 
 
