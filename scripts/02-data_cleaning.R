581dce11745091ba4a3503c4747a75f7042d2e99
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-01-19 04:25:00 +0000

    small edits to the cleaned_homeless_death_counts were made for easier use during graphing

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,9 +9,9 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(janitor)
 library(dplyr)
 library(lubridate)
+library(zoo)
 
 #### Clean data ####
 
@@ -33,6 +33,7 @@ cleaned_central_intake_call <-
 # then clean the raw_homeless_death_counts dataset
 raw_homeless_death_counts <- read_csv("inputs/data/raw_homeless_death_counts.csv")
 
+
 last_day_of_month = c(31,28,31,30,31,30,31,31,30,31,30,31)
 
 cleaned_homeless_death_counts <- raw_homeless_death_counts |> 
@@ -50,12 +51,15 @@ cleaned_homeless_death_counts <- raw_homeless_death_counts |>
   # Febuary of 2020 is a special month, there was 29 days in Feb of 2020
   mutate(Day = ifelse(row_number() == 38, 29, Day)) |> 
   # combining the year, month, and day together to form a date class
-  mutate(Date = make_date(Year, Month, Day))
+  mutate(Date = make_date(Year, Month, Day)) |>
+  # mutate a new column that will serve as the name displayed in graphs later
+  mutate(Date_Displayed = zoo:: as.yearmon(Date))
 
 
 # select only the data we want to use in analysis
 cleaned_homeless_death_counts <- cleaned_homeless_death_counts |> 
-  select(Date, Count)
+  select(Date_Displayed, Date, Count)
+cleaned_homeless_death_counts
 
 #### Save data ####
 # save as cleaned_central_intake_call
