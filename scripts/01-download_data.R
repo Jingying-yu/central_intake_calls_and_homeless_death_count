46ed17265d84aa18e500505cb1d5213fcdcf6404
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-01-19 00:28:08 +0000

    editted data download script to exclude "skip = 1". "skip = 1" caused the final dataset to not have the first row (title row)

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -19,8 +19,7 @@ library(tidyverse)
 raw_central_intake_call_wrap_up <-
   read_csv(
     file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/central-intake-calls/resource/4858b2dc-9b4e-425c-aaeb-ef23d819cbc4/download/Central%20Intake%20Call%20Wrap-Up%20Codes%20Data.csv",
-    show_col_types = FALSE,
-    skip = 1
+    show_col_types = FALSE
   )
 
 
@@ -28,8 +27,7 @@ raw_central_intake_call_wrap_up <-
 raw_homeless_death_counts <-
   read_csv(
     file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/dc4ec2fa-d655-46ca-af32-f216d26e9804/download/Homeless%20deaths%20by%20month.csv",
-    show_col_types = FALSE,
-    skip = 1
+    show_col_types = FALSE
   )
 
 
